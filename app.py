57801ab499110d5fdd03eccbcaccd2e9efea8ed5
thagZzz <142648362+thagZzz@users.noreply.github.com>
2023-10-19 09:56:04 +0000

    ..

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='BoBL')
+  return render_template('home.html', jobs=jobs, company_name="BoBL")
 
 
 @app.route("/api/jobs")
